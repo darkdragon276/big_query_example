a7940d459d9b2de9b833d0e5f8fa41bae415c388
Nam Chu Hai <chn5hc@bosch.com>
2023-10-29 01:00:55 +0700

    fix server https for openapi

--- src/main/java/com/backend/bigquery/BigQueryApplication.java
+++ src/main/java/com/backend/bigquery/BigQueryApplication.java
@@ -1,7 +1,10 @@
 package com.backend.bigquery;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 
 @SpringBootApplication
 //@EntityScan(basePackages = {"com.backend.bigquery.entity"})
